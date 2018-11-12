34e9cc4aa0ab5d8b9591651886be5c334adfcd4b
TitanSatya <37222144+TitanSatya@users.noreply.github.com>
2018-11-12 17:20:04 +0530

    Intial Changes
    Intial Changes

---
+++ LINQDefender/LINQDefender/LINQDefenderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDefender
+{
+    public static class LINQDefenderExtensions
+    {
+        public static T FirstOrDefaultOnNull<T>(this IEnumerable<T> collection) where T : new()
+        {
+            if (collection == null)
+            {
+                return new T();
+            }
+            else
+            {
+                return collection.FirstOrDefault();
+            }
+        }
+
+        public static T FirstOrDefaultOnNull<T>(this IEnumerable<T> collection, Func<T, bool> predicate) where T : new()
+        {
+            if (collection == null)
+            {
+                return new T();
+            }
+            else
+            {
+                return collection.FirstOrDefault(predicate);
+            }
+        }
+    }
+}
