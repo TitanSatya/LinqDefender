87d601752a5a8cbf131c093f65ef7a5ba6e2637f
TitanSatya <37222144+TitanSatya@users.noreply.github.com>
2018-11-12 19:03:00 +0530

    ANY Added
    Added Any Method, Change Default file name to Filename_FirstOrDefault

---
+++ LINQDefender/LINQDefender/LinqDefenderExtensions_Any.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDefender
+{
+    public static class LinqDefenderExtensions_Any
+    {
+        /// <summary>
+        /// If Any value is there then return true or return false.
+        /// If Collection is null then return false.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static bool AnyOnNull<T>(this IEnumerable<T> collection) where T : new()
+        {
+            if (collection == null)
+            {
+                return false;
+            }
+            else
+            {
+                return collection.Any();
+            }
+        }
+
+        /// <summary>
+        /// If Any value is there then return true or return false.
+        /// If Collection is null then return false.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static bool AnyOnNull<T>(this IEnumerable<T> collection, Func<T, bool> predicate) where T : new()
+        {
+            if (collection == null)
+            {
+                return false;
+            }
+            else
+            {
+                return collection.Any(predicate);
+            }
+        }
+    }
+}
